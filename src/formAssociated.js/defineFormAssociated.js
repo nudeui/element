0ed09a545eb55424d9945b1232e3c363cea5f8e6
Lea Verou <lea@verou.me>
2024-05-08 14:35:06 -0400

    [formAssociated] Internals should be constructed even if not read

--- src/formAssociated.js/defineFormAssociated.js
+++ src/formAssociated.js/defineFormAssociated.js
@@ -1,5 +1,4 @@
 import {
-	defineLazyProperty,
 	resolveValue,
 } from "../util.js";
 
@@ -11,18 +10,10 @@ export default function (Class, {
 	internalsProp = "_internals",
 	getters = ["labels", "form", "type", "name", "validity", "validationMessage", "willValidate"],
 } = Class.formAssociated) {
-	defineLazyProperty(Class.prototype, internalsProp, el => {
-		let source = resolveValue(like, [el, el]);
-		let internals = el.attachInternals?.();
-
-		if (internals) {
-			internals.ariaRole = role ?? source?.computedRole ?? "textbox";
-			internals.setFormValue(el[valueProp]);
-			el.addEventListener(changeEvent, () => internals.setFormValue(el[valueProp]));
-		}
-
-		return internals;
-	});
+	if (HTMLElement.prototype.attachInternals === undefined) {
+		// Not supported
+		return;
+	}
 
 	for (let prop of getters) {
 		Object.defineProperty(Class.prototype, prop, {
@@ -34,4 +25,25 @@ export default function (Class, {
 	}
 
 	Class.formAssociated = true;
+
+	let ret = function postConstruct () {
+		let internals = this[internalsProp] ??= this.attachInternals();
+
+		if (internals) {
+			let source = resolveValue(like, [this, this]);
+			role ??= source?.computedRole;
+
+			if (role) {
+				// XXX Should we set a default role? E.g. "textbox"?
+				internals.ariaRole = role ?? source?.computedRole;
+			}
+
+			internals.setFormValue(this[valueProp]);
+			el.addEventListener(changeEvent, () => internals.setFormValue(this[valueProp]));
+		}
+	}
+
+	Class.postConstruct?.push(ret);
+
+	return ret;
 }
