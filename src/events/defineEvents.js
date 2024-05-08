c64ca2a6bbfee9160ca05e9d5c22a56fe76e909f
Lea Verou <lea@verou.me>
2024-05-08 18:10:50 -0400

    Refactor events to minimize calls

--- src/events/defineEvents.js
+++ src/events/defineEvents.js
@@ -1,4 +1,4 @@
-import Props from "../props/Props.js";
+import defineProps from "../props/defineProps.js";
 import PropChangeEvent from "../common/PropChangeEvent.js";
 import {
 	pick,
@@ -13,77 +13,95 @@ import {
  * @param {object} options
  * @returns
  */
-export function defineEvent (Class, name, options = {}) {
-	let onName = `on${name}`;
-	let isImplemented = onName in Class.prototype;
-
-	if (!isImplemented) {
-		Props.add(Class, onName, {
-			type: Function,
-			typeOptions: {
-				arguments: ["event"],
-			},
-			reflect: {
-				from: true,
-			}
-		});
+function retargetEvent (name, from) {
+	if (typeof from === "function") {
+		from = { on: from };
 	}
 
-	if (isImplemented && !options.from) {
-		// Not much to do here
-		return;
-	}
+	let type = from?.type ?? name;
 
 	return function init () {
-		this.addEventListener("propchange", event => {
-			if (!isImplemented) {
-				if (event.name === onName) {
-					// Implement the oneventname attribute
-					let change = event.detail;
+		// Event is a subset of another event (either on this element or another element)
+		let target = resolveValue(from?.on ?? this, [this]);
+		let host = this;
 
-					if (change.oldInternalValue) {
-						this.removeEventListener(name, change.oldInternalValue);
-					}
+		target.addEventListener(type, event => {
+			if (!from.when || from.when(event)) {
+				let EventConstructor = from.event ?? event.constructor;
+				let source = from.constructor
+					// Construct specific event object
+					? pick(event, ["bubbles", "cancelable", "composed", "detail"])
+					// Retarget this event
+					: event;
+				let options = Object.assign({}, source, from.options);
 
-					if (change.parsedValue) {
-						this.addEventListener(name, change.parsedValue);
-					}
-				}
-			}
-
-			if (event.name === options.propchange) {
-				// Shortcut for events that fire when a specific prop changes
-				this.dispatchEvent(new PropChangeEvent(name, event));
+				let newEvent = new EventConstructor(name, options);
+				host.dispatchEvent(newEvent);
 			}
 		});
-
-		// Event is a subset of another event (either on this element or another element)
-		let from = typeof options.from === "function" ? { on: options.from } : options.from;
-
-		if (from) {
-			let target = resolveValue(from?.on ?? this, [this]);
-			let host = this;
-			let type = from?.type ?? name;
-
-			target.addEventListener(type, event => {
-				if (!from.when || from.when(event)) {
-					let EventConstructor = from.event ?? event.constructor;
-					let source = from.constructor
-						// Construct specific event object
-						? pick(event, ["bubbles", "cancelable", "composed", "detail"])
-						// Retarget this event
-						: event;
-					let options = Object.assign({}, source, from.options);
-
-					let newEvent = new EventConstructor(name, options);
-					host.dispatchEvent(newEvent);
-				}
-			});
-		}
 	}
 }
 
 export default function defineEvents (Class, events = Class.events) {
-	let fns = events.map(event => defineEvent(Class, event)).filter(Boolean);
+	let fns = [];
+
+	let props = Object.keys(events)
+		.filter(name => !("on" + name in Class.prototype))
+		.map(name => [
+			"on" + name,
+			{
+				type: Function,
+				typeOptions: {
+					arguments: ["event"],
+				},
+				reflect: {
+					from: true,
+				}
+			}
+		]);
+	let propchange = Object.entries(events)
+		.filter(([name, options]) => options.propchange)
+		.map(([name, options]) => [options.propchange, name]);
+
+	if (props.length > 0 || propchange.length > 0) {
+		props = Object.fromEntries(props);
+		propchange = Object.fromEntries(propchange);
+
+		defineProps(Class, props);
+		fns.push(function init () {
+			this.addEventListener("propchange", event => {
+				// Implement onEventName attributes/properties
+
+				if (props[event.name]) {
+					let eventName = event.name.slice(2);
+					let change = event.detail;
+
+					if (change.oldInternalValue) {
+						this.removeEventListener(eventName, change.oldInternalValue);
+					}
+
+					if (change.parsedValue) {
+						this.addEventListener(eventName, change.parsedValue);
+					}
+				}
+
+				if (propchange[event.name]) {
+					// Shortcut for events that fire when a specific prop changes
+					let eventName = propchange[event.name];
+					this.dispatchEvent(new PropChangeEvent(eventName, event));
+				}
+			});
+		})
+	}
+
+	for (let [name, options] of Object.entries(events)) {
+		if (options.from) {
+			let fn = retargetEvent(name, options.from);
+			if (fn) {
+				fns.push(fn);
+			}
+		}
+	}
+
 	return queueInitFunction(Class, fns);
 }
