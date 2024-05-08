b8c4f354d56367cca447208adcc9765c4680f8a7
Lea Verou <lea@verou.me>
2024-05-08 18:27:45 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-export { default } from "./Element.js";
-export { default as Props } from "./props/Props.js";
-export { default as Prop } from "./props/Prop.js";
-export { default as PropChangeEvent } from "./props/PropChangeEvent.js";
-export { default as defineEvents } from "./events/defineEvents.js";
-export { default as defineFormAssociated } from "./formAssociated.js/defineFormAssociated.js";
+export { default } from "./src/Element.js";
+export { default as Props } from "./src/props/Props.js";
+export { default as Prop } from "./src/props/Prop.js";
+export { default as PropChangeEvent } from "./src/props/PropChangeEvent.js";
+export { default as defineEvents } from "./src/events/defineEvents.js";
+export { default as defineFormAssociated } from "./src/formAssociated.js/defineFormAssociated.js";
