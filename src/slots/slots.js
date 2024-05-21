6bfa9c2bcf09c8e1f28b95c050e6ca46b6ee5a1c
Lea Verou <lea@verou.me>
2024-05-21 12:06:55 -0400

    First stab at slots mixin

---
+++ src/slots/slots.js
@@ -0,0 +1,62 @@
+import {
+	queueInitFunction,
+} from "../util.js";
+
+function assignSlots () {
+	let children = this.childNodes;
+	let slotElements = Object.values(this._slots);
+	let assignments = new WeakMap();
+
+	// Assign to slots
+	for (let child of children) {
+		let assignedSlot;
+
+		if (child.slot) {
+			// Explicit slot assignment by name, this takes priority
+			assignedSlot = this._slots[child.slot];
+		}
+		else if (child.matches) {
+			// Does child match any slot selector?
+			assignedSlot = slotElements.find(slot => child.matches(slot.dataset.assign));
+		}
+
+		assignedSlot ??= this._slots.default;
+		let all = assignments.get(assignedSlot) ?? new Set();
+		all.add(child);
+		assignments.set(assignedSlot, all);
+	}
+
+	for (let slot of slotElements) {
+		let all = assignments.get(slot) ?? new Set();
+		slot.assign(...all);
+	}
+}
+
+let mutationObserver;
+
+export default function (Class) {
+	// Class.prototype.assignSlots = assignSlots;
+
+	return queueInitFunction(function init () {
+		if (!this.shadowRoot) {
+			return;
+		}
+
+		for (let slot of this.shadowRoot.querySelectorAll("slot")) {
+			let name = slot.name ?? "default";
+			// This emulates how slots normally work: if there are duplicate names, the first one wins
+			// See https://codepen.io/leaverou/pen/KKLzBPJ
+			this._slots[name] ??= slot;
+		}
+
+		if (this.shadowRoot.slotAssignment === "manual") {
+			// slotchange won’t fire in this case, so we need to do this the old-fashioned way
+			mutationObserver ??= new MutationObserver(mutations => {
+				for (let mutation of mutations) {
+					assignSlots.call(mutation.target);
+				}
+			});
+			mutationObserver.observe(this, {childList: true});
+		}
+	});
+}
