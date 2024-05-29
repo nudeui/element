bdbcaa4f49a8a44c2e2621f0d30e9d23efb9ff25
Lea Verou <lea@verou.me>
2024-05-28 20:42:54 -0400

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,9 @@
+export { default as default} from "./Element.js";
+
+export { default as defineProps } from "./props/defineProps.js";
+export { default as defineEvents } from "./events/defineEvents.js";
+export { default as defineSlots } from "./slots/defineSlots.js";
+export { default as defineFormAssociated } from "./formAssociated/defineFormAssociated.js";
+
+export { default as defineMixin } from "./mixins/defineMixin.js";
+export { default as Hooks } from "./mixins/hooks.js";
