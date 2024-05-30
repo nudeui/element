288afb5f715ed3496dfe8f35fa36159d3020eefb
Lea Verou <lea@verou.me>
2024-05-30 15:16:12 -0400

    Add Set to supported types

--- src/props/types.js
+++ src/props/types.js
@@ -91,6 +91,25 @@ types.set(Function, {
 	stringify: false,
 });
 
+function parseList (value, { itemType, separator = ",", splitter } = {}) {
+	if (Array.isArray(value)) {
+		return value;
+	}
+
+	if (!splitter) {
+		// Make whitespace optional and flexible, unless the separator consists entirely of whitespace
+		let isSeparatorWhitespace = !separator.trim();
+		splitter = isSeparatorWhitespace ? /\s+/ : new RegExp(separator.replace(/\s+/g, "\\s*"));
+	}
+
+
+	value = typeof value === "string" ? value.trim().split(splitter) : [value];
+
+	if (itemType) {
+		value = value.map(item => parse(item, itemType));
+	}
+}
+
 types.set(Array, {
 	equals (a, b, { itemType } = {}) {
 		if (a.length !== b.length) {
@@ -99,21 +118,52 @@ types.set(Array, {
 
 		return a.every((item, i) => equals(item, b[i], itemType));
 	},
-	parse (value, { itemType, separator = ",", splitter } = {}) {
-		if (!Array.isArray(value)) {
-			if (!splitter) {
-				// Make whitespace optional and flexible, unless the separator consists entirely of whitespace
-				let isSeparatorWhitespace = !separator.trim();
-				splitter = isSeparatorWhitespace ? /\s+/ : new RegExp(separator.replace(/\s+/g, "\\s*"));
+	parse: parseList,
+	stringify: (value, { itemType, separator = ",", joiner } = {}) => {
+		if (itemType) {
+			value = value.map(item => stringify(item, itemType));
+		}
+
+		if (!joiner) {
+			let trimmedSeparator = separator.trim();
+			joiner = (!trimmedSeparator || trimmedSeparator === "," ? "" : " ") + separator + " ";
+		}
+
+		return value.join(joiner);
+	},
+});
+
+types.set(Set, {
+	equals (a, b, { itemType } = {}) {
+		if (a.size !== b.size) {
+			return false;
+		}
+
+		for (let item of a) {
+			if (!b.has(item)) {
+				return false;
+			}
+		}
+
+		return true;
+	},
+	parse (value, options) {
+		if (value instanceof Set) {
+			if (itemType) {
+				for (let item of value) {
+					let parsed = parse(item, itemType);
+					if (parsed !== item) {
+						value.delete(item);
+						value.add(parsed);
+					}
+				}
 			}
 
-
-			value = typeof value === "string" ? value.trim().split(splitter) : [value];
+			return value;
 		}
 
-		if (itemType) {
-			return value.map(item => parse(item, itemType));
-		}
+		let items = parseList(value, options);
+		return new Set(items);
 	},
 	stringify: (value, { itemType, separator = ",", joiner } = {}) => {
 		if (itemType) {
