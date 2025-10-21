79bed6046e1bd64df27758fbfd2a092fea0644a0
Lea Verou <lea@verou.me>
2025-10-21 15:56:14 -0400

    Update compose-functions.js
    Co-Authored-By: Dmitry Sharabin <dmitrysharabin@gmail.com>

--- src/util/compose-functions.js
+++ src/util/compose-functions.js
@@ -1,55 +1,58 @@
 import { ReversibleMap } from "./reversible-map.js";
 
+/**
+ * 1-1 mapping of original functions and their composed versions
+ * @type {Map<Function, Function>}
+ */
 export const composedFunctions = new ReversibleMap();
 export const functions = Symbol("Constituent functions");
 
 /**
  * Compose functions in a way that preserves the originals.
  * Will only ever produce one new function, even if called repeatedly
- * @param {Function} fn1
- * @param {Function} fn2
+ * @param {Function} keyFn
+ * @param {Function} ...fns
  * @returns {Function}
  */
-export function composeFunctions (fn1, fn2) {
-	let isComposed = composedFunctions.getKey(fn1);
-	let composedFn;
+export function composeFunctions (keyFn, ...fns) {
+	if (!keyFn) {
+		keyFn = fns.shift();
+	}
 
-	if (isComposed) {
+	if (fns.length === 0) {
+		// Nothing to do here
+		return keyFn;
+	}
+
+	let composedFn = composedFunctions.getKey(keyFn);
+
+	if (composedFn) {
 		// A composed function was provided instead of the constituent
-		composedFn = isComposed;
-		if (!composedFn[functions]) debugger;
-		fn1 = composedFn[functions][0];
+		keyFn = composedFn[functions][0];
 	}
 	else {
-		composedFn = composedFunctions.get(fn1);
+		composedFn = composedFunctions.get(keyFn);
 	}
 
 	if (!composedFn) {
+		// New composed function
 		composedFn = function (...args) {
-			let fns = composedFn[functions];
 			let ret;
-			for (let fn of fns) {
-				ret = fn.call(this, ...args) ?? ret;
+			for (let fn of composedFn[functions]) {
+				let localRet = fn.call(this, ...args);
+				if (ret === undefined) {
+					ret = localRet;
+				}
 			}
 			return ret;
 		};
 
-		composedFn[functions] = [fn1, fn2];
-		composedFunctions.set(fn1, composedFn);
+		composedFn[functions] = [keyFn];
+		composedFunctions.set(keyFn, composedFn);
 	}
-	else {
-		let prev = composedFn[functions].indexOf(fn2);
 
-		if (prev !== composedFn[functions].length - 1) {
-			if (prev < composedFn[functions].length - 1) {
-				// If already there, but not at the end, remove first
-				composedFn[functions].splice(prev, 1);
-			}
-
-			composedFn[functions].push(fn2);
-		}
-		// else Already at the end, nothing to do here
-	}
+	// Add new constituents
+	composedFn[functions].push(...fns.filter(fn => !composedFn[functions].includes(fn)));
 
 	return composedFn;
 }
