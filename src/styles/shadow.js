7b8ea2c95a73cccc077b5938eec16a6110e32390
Dmitry Sharabin <dmitrysharabin@gmail.com>
2025-10-31 00:49:23 +0100

    Address @LeaVerou's feedback

--- src/styles/shadow.js
+++ src/styles/shadow.js
@@ -6,7 +6,7 @@ import { adoptCSS, fetchCSS, getSupers, getSymbols } from "./util.js";
 const { fetchedStyles, initialized, render, init } = getSymbols;
 
 export function appliesTo (Class) {
-	return "shadowStyles" in Class;
+	return "styles" in Class;
 }
 
 export const Mixin = (Super = HTMLElement) => class ShadowStyles extends Super {
@@ -47,7 +47,7 @@ export const Mixin = (Super = HTMLElement) => class ShadowStyles extends Super {
 	static [init] () {
 		super[init]?.();
 
-		if (!this.shadowStyles || Object.hasOwn(this, initialized)) {
+		if (!this.styles || Object.hasOwn(this, initialized)) {
 			return;
 		}
 
@@ -56,11 +56,11 @@ export const Mixin = (Super = HTMLElement) => class ShadowStyles extends Super {
 		let supers = getSupers(this, HTMLElement);
 
 		for (let Class of supers) {
-			if (Object.hasOwn(Class, "shadowStyles") && !Object.hasOwn(Class, fetchedStyles)) {
+			if (Object.hasOwn(Class, "styles") && !Object.hasOwn(Class, fetchedStyles)) {
 				// Initiate fetching when the first element is constructed
-				let styles = (Class[fetchedStyles] = Array.isArray(Class["shadowStyles"])
-					? Class["shadowStyles"].slice()
-					: [Class["shadowStyles"]]);
+				let styles = (Class[fetchedStyles] = Array.isArray(Class["styles"])
+					? Class["styles"].slice()
+					: [Class["styles"]]);
 
 				for (let i = 0; i < styles.length; i++) {
 					styles[i] = fetchCSS(styles[i], Class.url);
