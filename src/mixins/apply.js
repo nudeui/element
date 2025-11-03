0ac87fa61d4d3d3b46add78387defa92eac3b140
Dmitry Sharabin <dmitrysharabin@gmail.com>
2025-11-03 13:17:08 +0100

    Add a rudimentary `init()` to a class if it doesn't have one

--- src/mixins/apply.js
+++ src/mixins/apply.js
@@ -1,4 +1,5 @@
 import { copyProperties } from "../util/copy-properties.js";
+import { getSuper } from "../util/super.js";
 
 export function applyMixins (Class = this, mixins = Class.mixins) {
 	if (Object.hasOwn(Class, "mixinsActive") || !mixins?.length) {
@@ -22,6 +23,13 @@ export function applyMixin (Class, Mixin, force = false) {
 	if (alreadyApplied && !force) {
 		// Already applied
 		return;
+	}
+
+	// If the class doesn't have an init() method, add a default one that calls super.init()
+	if (!Object.hasOwn(Class.prototype, "init")) {
+		Class.prototype.init = function init () {
+			getSuper(this, "init")?.call(this);
+		};
 	}
 
 	copyProperties(Class, Mixin, {recursive: true, prototypes: true});
