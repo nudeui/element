e4c3e31e2cf3cae98a59f410790df8f171815d1a
Lea Verou <lea@verou.me>
2025-12-08 18:41:56 -0500

    Refactor hooks, allow any casing for hook name
    - Allow kebab-case, camelCase OR underscore_case for hook name
    - Remove unused argument from `add()`
    - Remove unused signatures from `add()` (multiple callbacks at the same hook or the same callback on multiple hooks)
    - Remove unised logic from constructor (new Hooks(hooksObj))
    - New `Hook` class to manage hook metadata

--- src/mixins/hooks.js
+++ src/mixins/hooks.js
@@ -1,9 +1,8 @@
 export default class Hooks {
-	constructor (hooks) {
-		if (hooks instanceof Hooks) {
-			return hooks;
-		}
+	/** @type {Record<string, Hook>} */
+	hooks = {};
 
+	constructor (hooks) {
 		if (hooks) {
 			this.add(hooks);
 		}
@@ -11,36 +10,38 @@ export default class Hooks {
 
 	/**
 	 * Schedule one or more callbacks to be executed on one or more hooks
-	 * @param {*} name
-	 * @param {*} callback
-	 * @param {*} first
-	 * @returns
+	 *
+	 * @param {string} name
+	 * @param {function} callback
+	 * @void
+	 *
+	 * @overload
+	 * @param {Record<string, function>} hooks
+	 * @void
 	 */
-	add (name, callback, first) {
-		if (Array.isArray(name)) {
-			return name.map(name => this.add(name, callback, first));
-		}
-
-		if (Array.isArray(callback)) {
-			return callback.map(callback => this.add(name, callback, first));
+	add (name, callback) {
+		if (!name) {
+			return;
 		}
 
 		if (typeof name === "object") {
 			// Adding multiple hooks at once
 			let hooks = name;
-			first = callback;
 
 			for (let name in hooks) {
-				this.add(name, hooks[name], first);
+				this.add(name, hooks[name]);
 			}
 
 			return;
 		}
 
-		if (callback) {
-			this[name] ??= [];
-			this[name][first ? "unshift" : "push"](callback);
+		if (!callback) {
+			return;
 		}
+
+		name = Hooks.getCanonicalName(name);
+		this.hooks[name] ??= new Hook();
+		this.hooks[name].add(callback);
 	}
 
 	/**
@@ -49,12 +50,23 @@ export default class Hooks {
 	 * @param {object} [env]
 	 */
 	run (name, env) {
-		if (!this[name]) {
-			return;
-		}
-
-		this[name].forEach(function (callback) {
-			callback.call(env?.context ?? env, env);
-		});
+		name = Hooks.getCanonicalName(name);
+		this.hooks[name]?.run(env);
 	}
+
+	// Allow either camelCase, underscore_case or kebab-case for hook names
+	static getCanonicalName (name) {
+		return name.replace(/[A-Z]/g, letter => `-${letter.toLowerCase()}`).replace(/-/g, "_");
+	}
+}
+
+export class Hook extends Set {
+
+	run (env) {
+		for (let callback of this) {
+			let context = env?.context ?? env;
+			callback.call(context, env);
+		}
+	}
+
 }
