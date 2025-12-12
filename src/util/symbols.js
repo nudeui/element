0e968e97a3c1916bfc5547d8f59b51a40c4d75d6
Lea Verou <lea@verou.me>
2025-12-12 17:21:23 -0500

    Support multiple symbol registries

--- src/util/symbols.js
+++ src/util/symbols.js
@@ -10,23 +10,28 @@ export const newSymbols = new Proxy({}, {
 	},
 });
 
-export const newKnownSymbols = new Proxy({}, {
-	get (target, prop) {
-		if (typeof prop === "string") {
-			if (KNOWN_SYMBOLS[prop]) {
-				return KNOWN_SYMBOLS[prop];
+export function registry (knownSymbols) {
+	return new Proxy({}, {
+		get (target, prop) {
+			if (typeof prop === "string") {
+				if (knownSymbols[prop]) {
+					return knownSymbols[prop];
+				}
+
+				let ret = Symbol(prop);
+				knownSymbols[prop] = ret;
+				return ret;
 			}
 
-			let ret = Symbol(prop);
-			KNOWN_SYMBOLS[prop] = ret;
-			return ret;
-		}
+			return target[prop];
+		},
+	});
+}
 
-		return target[prop];
-	},
-});
+export const newKnownSymbols = registry(KNOWN_SYMBOLS);
 
 export default {
 	new: newSymbols,
 	known: newKnownSymbols,
+	registry,
 };
