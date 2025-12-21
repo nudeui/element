805603e34e803273409c4969053f66fd226b24c5
Lea Verou <lea@verou.me>
2025-12-21 16:11:23 +0100

    `/plugins` export

--- src/index-fn.js
+++ src/index-fn.js
@@ -5,6 +5,4 @@
 export { default as Element } from "./element/Element.js";
 export * from "./common-plugins.js";
 export { default as commonPlugins } from "./common-plugins.js";
-export { default as Hooks } from "./plugins/hooks.js";
-export { default as symbols } from "./plugins/symbols.js";
-export * from "./plugins/plugins.js";
+export * from "./plugins/index.js";
