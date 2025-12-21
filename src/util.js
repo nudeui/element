131f7ca3aded98df8d98c8849d0602009bb669eb
Lea Verou <lea@verou.me>
2025-12-21 16:06:05 +0100

    Move unused utils to `/util/unused`

--- src/util.js
+++ src/util.js
@@ -1,6 +1,4 @@
 export * from "./util/resolve-value.js";
-export * from "./util/is-class.js";
-export * from "./util/is-subclass-of.js";
 export * from "./util/lazy.js";
 export * from "./util/pick.js";
 export * from "./util/super.js";
