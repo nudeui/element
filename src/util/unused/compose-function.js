a39e9059a9a3f932226f809068074e7bd2f497bc
Lea Verou <lea@verou.me>
2025-12-21 12:16:42 +0100

    Add unused helper

---
+++ src/util/unused/compose-function.js
@@ -0,0 +1,49 @@
+/**
+ * Wrap a function with another function that can be extended with side effects at any point.
+ * Side effects run before the function body, in the order they are added.
+ * @param {function} body - The function to wrap
+ * @param  {...function} sideEffects
+ * @returns {function}
+ */
+export const sideEffects = Symbol("Side effects");
+export const mutable = Symbol("Mutable");
+
+export function composeFunction (body, ...sideEffectFns) {
+	let mutableFn = body[sideEffects] ? body : body[mutable];
+
+	if (!mutableFn) {
+		// First time extending body
+		let name = body.name || "";
+
+		// Wrap in object so we can dynamically assign a name
+		// https://dev.to/tmikeschu/dynamically-assigning-a-function-name-in-javascript-2d70
+		let wrapper = {
+			[name] (...args) {
+				let ret = body.apply(this, args);
+
+				for (let sideEffect of mutableFn[sideEffects]) {
+					sideEffect.apply(this, args);
+				}
+
+				return ret;
+			},
+		};
+
+		mutableFn = body[mutable] = wrapper[name];
+		mutableFn.body = body;
+		mutableFn[sideEffects] = new Set();
+	}
+
+	body = mutableFn.body;
+
+	for (const sideEffect of sideEffectFns) {
+		if (body === sideEffect) {
+			// The function cannot be a side effect of itself
+			continue;
+		}
+
+		mutableFn[sideEffects].add(sideEffect);
+	}
+
+	return mutableFn;
+}
