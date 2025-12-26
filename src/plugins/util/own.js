71283f5ed71aad07ad97e7f0c292322289968038
Lea Verou <lea@verou.me>
2025-12-26 19:31:47 +0200

    [own] Return early if already called

--- src/plugins/util/own.js
+++ src/plugins/util/own.js
@@ -1,3 +1,5 @@
+const own = Symbol("own");
+
 /**
  * Define static properties that do not inherit (every subclass has its own value)
  * @param {*} obj
@@ -5,37 +7,43 @@
  * @param {*} options
  */
 export function defineOwnProperty (obj, name, options = {}) {
+	let existingDescriptor = Object.getOwnPropertyDescriptor(obj, name);
+
+	if (existingDescriptor?.get?.[own]) {
+		// Already defined
+		return;
+	}
+
 	if (typeof options === "function") {
 		options = { get: options };
 	}
 
 	let _name = options.internal ?? Symbol(name);
-	let { enumerable = true, configurable = true } = options;
-
-	let existingDescriptor = Object.getOwnPropertyDescriptor(obj, name);
 
 	if (existingDescriptor && !Object.hasOwn(obj, _name)) {
 		Object.defineProperty(obj, _name, existingDescriptor);
 	}
 
-	Object.defineProperty(obj, name, {
-		get () {
-			if (!Object.hasOwn(this, _name)) {
-				this[_name] = options.get.call(this);
-			}
+	function get () {
+		if (!Object.hasOwn(this, _name)) {
+			this[_name] = options.get.call(this);
+		}
 
-			return this[_name];
-		},
-		set (value) {
-			if (options.writable === false && Object.hasOwn(this, _name) && this[_name] !== value) {
-				throw new Error(`Cannot set read-only property ${name}`);
-			}
+		return this[_name];
+	}
 
-			this[_name] = value;
-		},
-		configurable,
-		enumerable,
-	});
+	function set (value) {
+		if (options.writable === false && Object.hasOwn(this, _name) && this[_name] !== value) {
+			throw new Error(`Cannot set read-only property ${name}`);
+		}
+
+		this[_name] = value;
+	}
+
+	get[own] = true;
+
+	let { enumerable = true, configurable = true } = options;
+	Object.defineProperty(obj, name, { get, set, configurable, enumerable });
 }
 
 export function defineOwnProperties (obj, properties) {
