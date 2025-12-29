7af5a90cd2a56773d12f99a207181b9112f84f28
Lea Verou <lea@verou.me>
2025-12-29 14:04:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,2 @@
-export { default } from "./src/Element.js";
-export { default as Props } from "./src/props/Props.js";
-export { default as Prop } from "./src/props/Prop.js";
-export { default as PropChangeEvent } from "./src/props/PropChangeEvent.js";
-export { default as defineEvents } from "./src/events/defineEvents.js";
-export { default as defineFormBehavior } from "./src/formBehavior.js/defineFormBehavior.js";
+export { default as default } from "./src/index.js";
+export * from "./src/index.js";
