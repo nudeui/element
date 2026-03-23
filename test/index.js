c13c8913cbfeb76573eb13b9a87d7b4e06109b9d
Dmitry Sharabin <dmitrysharabin@gmail.com>
2026-03-23 13:11:15 +0100

    Add entry point

---
+++ test/index.js
@@ -0,0 +1,9 @@
+import Prop from "./Prop.js";
+import Props from "./Props.js";
+import hooks from "./hooks.js";
+import split from "./split.js";
+
+export default {
+	name: "All tests",
+	tests: [Prop, Props, hooks, split],
+};
